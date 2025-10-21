355024158691c710baec095596af2d86b5c80899
Nirmaljohn24 <johnsondravid143@gmail.com>
2025-10-21 15:47:36 +0530

    first commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 // Firebase configuration
 // Users need to replace these with their own Firebase config
 const firebaseConfig = {
- 
+  apiKey: "AIzaSyD8bw6oKZxTZEsf0NqLEFu3-KwWWg_xQLM",
+  authDomain: "productmanagement-a8c02.firebaseapp.com",
+  projectId: "productmanagement-a8c02",
+  storageBucket: "productmanagement-a8c02.firebasestorage.app",
+  messagingSenderId: "282895874009",
+  appId: "1:282895874009:web:dba8d268e92d5af2d28bc8",
+  measurementId: "G-98MWEZNTQ8"
 };
 
 // Initialize Firebase
